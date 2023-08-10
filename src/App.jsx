9a7cf4f1f64e357a6963e69409803714ff1ba981
aisc07 <amani.r.taylor@gmail.com>
2023-08-09 21:15:17 -0700

    added data w/useState and css style

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,42 @@ import { puppyList } from './data'
 import { useState } from 'react'
 import './App.css'
 
-function App() {
- const [puppies, setPuppies] = useState(puppyList)
-// console.log(puppies)
-console.log("puppyList: ", puppyList)
 
+function App() {
+ const [puppies, setPuppies] = useState(puppyList);
+ const [featPupId, setFeatPupId] = useState(null);
+ const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+//  {()=>{console.log("puppy id: ", puppy.id)}}
+ console.log(featuredPup);
+// console.log(puppies);
+// console.log("puppyList: ", puppyList);
+// console.log(featPupId.name)
   return (
-    
-      <div className="App">
+   
+    <div className="App">
+      <div className="title"><h1>Puppy Pals</h1></div>
+        
+
         {
           puppies.map((puppy)=>{
-            return <p key={puppy.id}>{puppy.name}</p>
+              return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
-      </div>
 
+
+        { 
+          featPupId && (
+            <div className="puppyInfo"> 
+                <h2 className="puppyName">{featuredPup.name}</h2>
+                    <ul className="list">
+                        <li>Age: {featuredPup.age}</li>
+                        <li>Email: {featuredPup.email}</li>
+                    </ul>
+            </div>
+          )
+        }
+
+    </div>
   )
 }
 
